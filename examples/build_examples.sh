c0de4a2d24ef370f5e53a9a6b7caae0203d22090
James Tauber <jtauber@jtauber.com>
2025-09-09 15:45:46 -0400

    script to build examples

---
+++ examples/build_examples.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+(cd example-01 && ./build.py)
+(cd example-02 && ./build.py)
+(cd example-03 && ./build.py)
+(cd example-04 && ./build.py)
+(cd example-05 && ./build.py)
+(cd example-06 && ./build.py)
