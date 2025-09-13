fcfffac226d8be4f9b6f220697ec3e1cf1fb964f
James Tauber <jtauber@jtauber.com>
2025-09-12 23:04:40 -0400

    use for to build all examples

--- examples/build_examples.sh
+++ examples/build_examples.sh
@@ -2,12 +2,10 @@
 
 export COVERAGE_FILE="$(pwd)/.coverage"
 coverage erase
-(cd example-01 && coverage run -a ./build.py)
-(cd example-02 && coverage run -a ./build.py)
-(cd example-03 && coverage run -a ./build.py)
-(cd example-04 && coverage run -a ./build.py)
-(cd example-05 && coverage run -a ./build.py)
-(cd example-06 && coverage run -a ./build.py)
-(cd example-07 && coverage run -a ./build.py)
-(cd example-08 && coverage run -a ./build.py)
-(cd example-09 && coverage run -a ./build.py)
+
+for dir in example-*; do
+    (cd "$dir" && coverage run -a --omit="./build.py" ./build.py)
+done
+
+coverage report -m
+coverage html
